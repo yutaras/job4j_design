6d7c374dbf0ae79d8b12df0a7656b0bc10110495
Yuriy Tarasenko <maxjeff@mail.ru>
2021-09-25 15:23:31 +0300

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/GenericsClass.java
@@ -0,0 +1,20 @@
+package ru.job4j.generics;
+
+public class GenericsClass<K, V> {
+    private K key;
+
+    private V value;
+
+    public GenericsClass(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "GenericsClass{"
+                + "key=" + key
+                + ", value=" + value
+                + '}';
+    }
+}
