36ecdee8e9a2bd36cc42e481f5994ca3ba553cfa
Yuriy Tarasenko <maxjeff@mail.ru>
2021-10-11 15:17:42 +0300

    2. Создать контейнер на базе связанного списка [#159]

--- src/main/java/ru/job4j/list/SimpleLinkedList.java
+++ src/main/java/ru/job4j/list/SimpleLinkedList.java
@@ -7,15 +7,15 @@ import java.util.Objects;
 
 public class SimpleLinkedList<E> implements List<E> {
 
-    transient int size = 0;
-    transient Node<E> first;
-    transient Node<E> last;
-    transient int modCount = 0;
+    private int size = 0;
+    private Node<E> first;
+    private Node<E> last;
+    private int modCount = 0;
 
     @Override
     public void add(E value) {
         final Node<E> l = last;
-        last = new Node<>(l, value, null);
+        last = new Node<>(value, null);
         if (l == null) {
             first = last;
         } else {
@@ -30,20 +30,16 @@ public class SimpleLinkedList<E> implements List<E> {
         Objects.checkIndex(index, size);
         Node<E> target = first;
         for (int i = 0; i < index; i++) {
-            target = getNextElement(target);
+            target = target.nextNode;
         }
-        return target.getElement();
-    }
-
-    private Node<E> getNextElement(Node<E> current) {
-        return current.getNextNode();
+        return target.element;
     }
 
     @Override
     public Iterator<E> iterator() {
         return new Iterator<>() {
-            final int expectedModCount = modCount;
-            Node<E> current = first;
+            private final int expectedModCount = modCount;
+            private Node<E> current = first;
 
             @Override
             public boolean hasNext() {
@@ -67,20 +63,12 @@ public class SimpleLinkedList<E> implements List<E> {
     }
 
     private static class Node<E> {
-        E element;
-        Node<E> nextNode;
+        private  E element;
+        private Node<E> nextNode;
 
-        Node(Node<E> prevNode, E element, Node<E> nextNode) {
+        Node(E element, Node<E> nextNode) {
             this.element = element;
             this.nextNode = nextNode;
-        }
-
-        public E getElement() {
-            return element;
-        }
-
-        public Node<E> getNextNode() {
-            return nextNode;
         }
 
     }
