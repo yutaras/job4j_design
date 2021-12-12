f19e7b27c13406a363916aa0fb013620467671cd
Yuriy Tarasenko <maxjeff@mail.ru>
2021-12-12 22:51:25 +0300

    5.2. Архивировать проект [#861 #239170]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public static void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path file : sources) {
+                zip.putNextEntry(new ZipEntry(String.valueOf(file)));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(String.valueOf(file)))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Search.validation(args);
+        ArgsName names = ArgsName.of(args);
+        Path dir = Paths.get(names.get("d"));
+        String ex = names.get("e");
+        List<Path> list = Search.search(dir, p -> !p.toFile().getName().endsWith(ex));
+        File out = new File(names.get("o"));
+        packFiles(list, out);
+    }
+}
