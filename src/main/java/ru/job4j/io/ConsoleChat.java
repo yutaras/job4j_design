f0d77df34d9fc966ba61c75f6892a0e61a5bebb6
Yuriy Tarasenko <maxjeff@mail.ru>
2021-12-22 14:55:48 +0300

    6. Кодировка. [#862 #239841]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,11 +1,10 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Random;
 
 public class ConsoleChat {
 
@@ -22,14 +21,35 @@ public class ConsoleChat {
     }
 
     public void run() {
-        Scanner input = new Scanner(System.in);
-        while (!input.equals("Стоп")) {
-
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            List<String> log = new ArrayList<>();
+            boolean flag = true;
+            String line = null;
+            Random rand = new Random();
+            while (!(OUT).equals(line)) {
+                line = reader.readLine();
+                log.add(line);
+                if ((STOP).equals(line)) {
+                    flag = false;
+                }
+                if ((CONTINUE).equals(line)) {
+                    flag = true;
+                }
+                if (flag) {
+                    String randomAnswerBot = readPhrases().get(rand.nextInt(readPhrases().size()));
+                    System.out.println(randomAnswerBot);
+                    log.add(randomAnswerBot);
+                }
+            }
+            saveLog(log);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 
     private List<String> readPhrases() {
-        try (BufferedReader in = new BufferedReader(new FileReader(this.path))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(this.botAnswers))) {
             for (String line = in.readLine(); line != null; line = in.readLine()) {
                 listOfPhrases.add(line);
             }
@@ -40,11 +60,17 @@ public class ConsoleChat {
     }
 
     private void saveLog(List<String> log) {
-
+        if (!log.isEmpty()) {
+            try (PrintWriter out = new PrintWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+                log.forEach(out::println);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
-        ConsoleChat cc = new ConsoleChat("", "");
+        ConsoleChat cc = new ConsoleChat("C:\\projects\\job4j_design\\data\\log.txt", "C:\\projects\\job4j_design\\data\\botAnswers.txt");
         cc.run();
     }
 }
