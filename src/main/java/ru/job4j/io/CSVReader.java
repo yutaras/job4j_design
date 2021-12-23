7a93058215b578d60f638f4de84d7c91431fe2e8
Yuriy Tarasenko <maxjeff@mail.ru>
2021-12-23 18:50:39 +0300

    7. Scanner [#504791 #243268]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) {
@@ -8,7 +9,15 @@ public class CSVReader {
              PrintWriter writer = new PrintWriter(new FileOutputStream(argsName.get("out")))) {
             while (in.ready()) {
                 String line = in.readLine();
-                writer.println(line.split(";")[0] + ";" + line.split(";")[1]);
+                var scanner = new Scanner(new ByteArrayInputStream(line.getBytes()))
+                        .useDelimiter(";");
+                while (scanner.hasNext()) {
+                    writer.print(scanner.next());
+                    writer.print(";");
+                    writer.println(scanner.next());
+                    scanner.next();
+                    scanner.next();
+                }
             }
         } catch (
                 IOException e) {
@@ -17,11 +26,10 @@ public class CSVReader {
     }
 
     public static void main(String[] args) throws Exception {
-        File file = new File("./data/source.csv");
-        File target = new File("./data/target.csv");
-        ArgsName argsName = ArgsName.of(new String[]{
-                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(), "-filter=name,age"
-        });
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Root folder is incorrect");
+        }
+        ArgsName argsName = ArgsName.of(args);
         handle(argsName);
     }
 }
