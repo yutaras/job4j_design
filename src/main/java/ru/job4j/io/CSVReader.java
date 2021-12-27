94d53ad0cc9be12bcd982cba7d32e4df754e0b86
Yuriy Tarasenko <maxjeff@mail.ru>
2021-12-27 13:07:51 +0300

    7. Scanner [#504791 #243268]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,23 +1,32 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) {
         try (BufferedReader in = new BufferedReader(new FileReader(argsName.get("path")));
              PrintWriter writer = new PrintWriter(new FileOutputStream(argsName.get("out")))) {
-            while (in.ready()) {
-                String line = in.readLine();
-                var scanner = new Scanner(new ByteArrayInputStream(line.getBytes()))
-                        .useDelimiter(";");
-                while (scanner.hasNext()) {
-                    writer.print(scanner.next());
-                    writer.print(";");
-                    writer.println(scanner.next());
-                    scanner.next();
-                    scanner.next();
+            String[] valueOfFilter = argsName.get("filter").split(",");
+            String line = in.readLine();
+            int[] arrayOfNumber = new int[valueOfFilter.length];
+            String[] valueOfColumnName = line.split(";");
+            for (int i = 0; i < valueOfFilter.length; i++) {
+                for (int j = 0; j < valueOfColumnName.length; j++) {
+                    if (valueOfFilter[i].equals(valueOfColumnName[j])) {
+                        arrayOfNumber[i] = j;
+                    }
                 }
+            }
+            for (int i = 0; i < arrayOfNumber.length - 1; i++) {
+                writer.print(line.split(";")[arrayOfNumber[i]] + ";");
+            }
+            writer.println(line.split(";")[arrayOfNumber.length - 1]);
+            while (in.ready()) {
+                String string = in.readLine();
+                for (int i = 0; i < arrayOfNumber.length - 1; i++) {
+                    writer.print(string.split(";")[arrayOfNumber[i]] + ";");
+                }
+                writer.println(string.split(";")[arrayOfNumber.length - 1]);
             }
         } catch (
                 IOException e) {
