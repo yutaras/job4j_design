bfcb836557a7940753cd94cc5cc5261dc2f486a4
Yuriy Tarasenko <maxjeff@mail.ru>
2022-01-04 20:28:20 +0300

    4. Slf4j - вывод exception. [#268853 #246746]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -3,8 +3,6 @@ package ru.job4j.io;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-
 public class UsageLog4j {
 
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
