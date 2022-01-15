fdc3085995890dbe6276bfb168695d577416dda2
Yuriy Tarasenko <maxjeff@mail.ru>
2022-01-15 18:00:55 +0300

    0. Что такое база данных. [#6860]

--- data/auto.sql
+++ data/auto.sql
@@ -1,4 +1,9 @@
-
+create table auto (
+    id serial primary key, 
+    name varchar(255),
+	power int,
+	type text
+);
 insert into auto(name, power, type) values('Ferarri', '400', 'X')
 select * from auto
 update auto set name ='BMW'
