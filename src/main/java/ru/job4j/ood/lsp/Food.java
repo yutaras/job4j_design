59597f8f4ea656f01d37bc827be7cc8e1af19eae
Yuriy Tarasenko <maxjeff@mail.ru>
2022-04-13 11:45:36 +0300

    1. Хранилище продуктов [#852 #287789]

--- src/main/java/ru/job4j/ood/lsp/Food.java
+++ src/main/java/ru/job4j/ood/lsp/Food.java
@@ -18,44 +18,8 @@ public class Food {
         this.discount = discount;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(LocalDate expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-
-    public LocalDate getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(LocalDate createDate) {
-        this.createDate = createDate;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
     public void setPrice(double price) {
         this.price = price;
-    }
-
-    public double getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(double discount) {
-        this.discount = discount;
     }
 
     @Override
