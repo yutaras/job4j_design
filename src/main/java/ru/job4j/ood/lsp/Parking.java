1b8ab85c90675e6bac85ee8ca42ffa06b0a61b3d
Yuriy Tarasenko <maxjeff@mail.ru>
2022-04-13 17:54:39 +0300

    2. Парковка машин [#853 #288433]

---
+++ src/main/java/ru/job4j/ood/lsp/Parking.java
@@ -0,0 +1,5 @@
+package ru.job4j.ood.lsp;
+
+public interface Parking {
+    boolean add(Vehicles vehicles);
+}
---
+++ src/main/java/ru/job4j/ood/lsp/Vehicles.java
@@ -0,0 +1,5 @@
+package ru.job4j.ood.lsp;
+
+public interface Vehicles {
+    int getSize();
+}
