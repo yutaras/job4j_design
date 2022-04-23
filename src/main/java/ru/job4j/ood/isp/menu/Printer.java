277d71a151e769dff9c5b26781ff146b52fd1694
Yuriy Tarasenko <maxjeff@mail.ru>
2022-04-23 12:27:38 +0300

    1. Создать меню. [#4748 #288899]

--- src/main/java/ru/job4j/ood/isp/menu/Printer.java
+++ src/main/java/ru/job4j/ood/isp/menu/Printer.java
@@ -3,14 +3,14 @@ package ru.job4j.ood.isp.menu;
 import java.util.Collections;
 
 public class Printer implements MenuPrinter {
+    private static final String STR = "--";
 
     @Override
     public void print(Menu menu) {
         for (Menu.MenuItemInfo info : menu) {
-            String str = "--" + info.getNumber() + info.getName();
+            String str = STR + info.getNumber() + info.getName();
             int length = info.getNumber().length();
-            String s = "--";
-            str = str.replace(s, String.join("", Collections.nCopies(length, s)));
+            str = str.replace(STR, String.join("", Collections.nCopies(length, STR)));
             System.out.println(str);
         }
     }
--- src/main/java/ru/job4j/ood/isp/menu/TODOApp.java
+++ src/main/java/ru/job4j/ood/isp/menu/TODOApp.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class TODOApp {
     public static final ActionDelegate STUB_ACTION = System.out::println;
+    private static final int ADD = 0;
+    private static final int SELECT = 1;
+    private static final int OUTPUT_ALL = 2;
+    private static final int EXIT = 3;
 
     public void init(Scanner scanner) {
         Menu menu = new SimpleMenu();
@@ -12,21 +16,21 @@ public class TODOApp {
             showMenu();
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            if (select == 0) {
+            if (select == ADD) {
                 System.out.println("Enter name parent");
                 String parent = scanner.next();
                 System.out.println("Enter child name");
                 String child = scanner.next();
                 menu.add(parent, child, STUB_ACTION);
-            } else if (select == 1) {
+            } else if (select == SELECT) {
                 System.out.println("Enter child name");
                 String child = scanner.next();
                 System.out.println(menu.select(child));
-            } else if (select == 2) {
+            } else if (select == OUTPUT_ALL) {
                 System.out.println("Output All");
                 Printer printer = new Printer();
                 printer.print(menu);
-            } else if (select == 3) {
+            } else if (select == EXIT) {
                 run = false;
             }
         }
